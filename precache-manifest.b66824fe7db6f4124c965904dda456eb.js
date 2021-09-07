725590d1be6e3bd6a762025a221f100f82ead8e8
Ciorap88 <vlad.014916@gmail.com>
2021-09-07 10:43:59 +0300

    Updates

---
+++ precache-manifest.b66824fe7db6f4124c965904dda456eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a62b4f7c1dd6d01a83d8939b43767ca",
+    "url": "/sudoku-solver/index.html"
+  },
+  {
+    "revision": "e504499036413915048e",
+    "url": "/sudoku-solver/static/css/main.24cd662c.chunk.css"
+  },
+  {
+    "revision": "c988be97c69a80b4c3cb",
+    "url": "/sudoku-solver/static/js/2.a753b939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku-solver/static/js/2.a753b939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e504499036413915048e",
+    "url": "/sudoku-solver/static/js/main.d5d8275c.chunk.js"
+  },
+  {
+    "revision": "caef4e9cdd5a2ce3fa22",
+    "url": "/sudoku-solver/static/js/runtime-main.e69d6014.js"
+  }
+]);
